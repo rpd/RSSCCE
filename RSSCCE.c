0eca6c5050b971d06a0df13f32fad2036a6dab26
Richard Dickinson <richard@dickinson350.freeserve.co.uk>
2018-02-23 22:38:19 +0000

    Create RSSCCE.c

---
+++ RSSCCE.c
@@ -0,0 +1,8 @@
+/*RSSCCE: Richard & Sean's Simple C Chess Engine A  basic C programmed basic chess engine.*/
+#include <stdio.h>
+
+
+printf("RSSCCEv1.0");
+int main(int argc, char **argv)
+{
+return 0;}
