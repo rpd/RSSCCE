b926bae1b09515384a44f2da46443dc7273852db
Richard Dickinson <richard@dickinson350.freeserve.co.uk>
2018-02-24 01:10:28 +0000

    Update rsscce.h

--- rsscce.h
+++ rsscce.h
@@ -1,1 +1,25 @@
 
+/* Description of a chess move */
+
+typedef struct {	int from;	int to;} Move;
+
+/* constants */
+#define SIDENUM		    64
+#define SIDEPLAYERS		8
+#define PIECENUM		6
+#define WHITE			0
+#define BLACK			1
+#define PAWN			0
+#define KNIGHT			1
+#define BISHOP			2
+#define ROOK			3
+#define QUEEN			4
+#define KING				5
+#define EMPTY			6
+/* macro */
+#define RANKS(x)		(x >> 3)
+#define FILES(x)		(x & 7)
+/* Miscellaneous */
+#define BOOL			int
+#define TRUE			1
+#define FALSE			0
